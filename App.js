61701a68d918a1ce783078e5d323ea8575d5fa7b
abGhost.cyber <xplendo@gmail.com>
2021-03-16 14:07:37 +0800

    added animatons

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  SafeAreaView,
-  ActivityIndicator,
-  View,
-  Text,
-} from "react-native";
+import React, { useRef, useState } from "react";
+import { StyleSheet, SafeAreaView, Text, Animated } from "react-native";
 import { WebView } from "react-native-webview";
 import AnimatedLoader from "react-native-animated-loader";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
   const [visible, setVisible] = useState(false);
+  const [fontIsLoaded, setFontIsLoaded] = useState(false);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
 
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setVisible((prevState) => !prevState);
-  //   }, 2000);
-  // });
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      "product-sans-bold": require("./assets/fonts/ProductSans-Bold.ttf"),
+    });
+  };
+
+  if (!fontIsLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontIsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  const fadeOut = () => {
+    Animated.loop(
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true,
+      })
+    ).start();
+  };
+
   const ActivityIndicatorElement = () => {
     return (
       <AnimatedLoader
@@ -28,7 +45,13 @@ export default function App() {
         animationStyle={styles.lottie}
         speed={1}
       >
-        <Text>Hang in there...</Text>
+        <Animated.View style={{ opacity: fadeAnim }}>
+          <Text
+            style={{ fontFamily: "product-sans-bold", textAlign: "center" }}
+          >
+          Swapping time and space...👩🏿‍💻
+          </Text>
+        </Animated.View>
       </AnimatedLoader>
     );
   };
@@ -40,7 +63,10 @@ export default function App() {
         javaScriptEnabled={true}
         //enable cache
         domStorageEnabled={true}
-        onLoadStart={() => setVisible(true)}
+        onLoadStart={() => {
+          setVisible(true);
+          fadeOut();
+        }}
         onLoad={() => setVisible(false)}
       />
       {visible ? <ActivityIndicatorElement /> : null}
